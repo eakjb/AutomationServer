73b249c65cfd5109aa3f8132a36e9d03dbe36954
jakebillings <jake@eakjb.com>
2015-08-03 10:26:31 -0600

    Enable CORS

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
