d686d17efd1a0ec03cf863c15312e4b5755cb97e
jakebillings <jake@eakjb.com>
2015-08-10 19:38:19 -0600

    Bug fixes for task manager

--- js/tasks.local.js
+++ js/tasks.local.js
@@ -9,8 +9,49 @@ var tasks = [];
 tasks.push({
   name: 'Turn on Lights in Jake\'s Room in morning',
   enabled: true,
-  shouldRun: util.time('TEST'),
-  run: util.state('Jake\'s Node (fake)',2,'on',console.log)
+  shouldRun: util.time({
+    hour:6,
+    minute: 30,
+    second: 0,
+    daysOfWeek:util.WEEK_DAYS
+  }),
+  run: util.state('JakeRoom',2,'on',console.log)
+});
+
+tasks.push({
+  name: 'Turn off Lights in Jake\'s Room in morning',
+  enabled: true,
+  shouldRun: util.time({
+    hour:7,
+    minute: 5,
+    second: 0,
+    daysOfWeek:util.FULL_WEEK
+    }),
+  run: util.state('JakeRoom',2,'off',console.log)
+});
+
+tasks.push({
+  name: 'Turn on Lights in Jake\'s Room on School Evenings',
+  enabled: true,
+  shouldRun: util.time({
+    hour:19,
+    minute: 30,
+    second: 0,
+    daysOfWeek:util.SCHOOL_DAYS
+  }),
+  run: util.state('JakeRoom',2,'on',console.log)
+});
+
+tasks.push({
+  name: 'Turn off Lights in Jake\'s Room on School Nights',
+  enabled: true,
+  shouldRun: util.time({
+    hour:22,
+    minute: 0,
+    second: 0,
+    daysOfWeek:util.SCHOOL_DAYS
+  }),
+  run: util.state('JakeRoom',2,'off',console.log)
 });
 
 module.exports = tasks;
--- js/util.js
+++ js/util.js
@@ -15,21 +15,29 @@ module.exports.delay = function (length, and) {
   }
 };
 
-module.exports.time = function (hour, minute, second, and) {
-  if (hour === 'TEST') {
+module.exports.WEEK_DAYS = [1,2,3,4,5];
+module.exports.WEEKENDS = [0,6];
+module.exports.FULL_WEEK = [0,1,2,3,4,5,6];
+module.exports.SCHOOL_DAYS = [0,1,2,3,4];
+
+module.exports.time = function (time, and) {
+  if (time === 'TEST') {
     var now = new Date();
-    hour = now.getHours();
-    minute = now.getMinutes();
-    second = now.getSeconds() + 1;
+    time = {};
+    time.hour = now.getHours();
+    time.minute = now.getMinutes();
+    time.second = now.getSeconds() + 1;
+    time.daysOfWeek = module.exports.FULL_WEEK;
   }
-  console.log('Running task at ' + hour + ':' + minute + ':' + second);
+  //console.log('Running task at ' + hour + ':' + minute + ':' + second);
   return function (env) {
     if ((and && and(env)) || !and) {
       return (env.lastRun === 'NEVER' ||
-        env.now.getTime() > env.lastRun.getTime() + 1000) &&
-        env.now.getHours() === hour &&
-        env.now.getMinutes() === minute &&
-        env.now.getSeconds() === second;
+      (env.now.getTime() > env.lastRun.getTime() + 1000)) &&
+        env.now.getHours() === time.hour &&
+        env.now.getMinutes() === time.minute &&
+        env.now.getSeconds() === time.second &&
+        (time.daysOfWeek.indexOf(env.now.getDay()) >= 0);
     }
     return false;
   };
@@ -44,46 +52,48 @@ module.exports.state = function (nodeName, output_id, state, onError) {
       if (data) {
         var datum = data[0];
 
-        console.log(data);
+        //console.log(data);
 
-        request(datum.address + '/outputs', function (error, response, body) {
-          if (error && onError) {
-            onError(error);
-          }
-
-          var outputs = JSON.parse(body).data;
-
-          console.log(outputs);
-
-          outputs.forEach(function (output) {
-            if (output.output_id === output_id) {
-              var addr = datum.address + '/outputs/' + output_id + '/state';
-              var payload = {
-                value: output.states[state].value
-              };
-
-              console.log('PUT', addr, payload);
-
-              request({
-                url: addr,
-                body:JSON.stringify(payload),
-                method: 'PUT'
-
-              }, function (error, response, body) {
-
-                if (error && onError) {
-                  onError(error);
-                }
-
-                console.log(response.statusCode);
-
-              });
-
-
+        if (datum) {
+          request(datum.address + '/outputs', function (error, response, body) {
+            if (error && onError) {
+              onError(error);
             }
-          });
 
-        });
+            var outputs = JSON.parse(body).data;
+
+            //console.log(outputs);
+
+            outputs.forEach(function (output) {
+              if (output.output_id === output_id) {
+                var addr = datum.address + '/outputs/' + output_id + '/state';
+                var payload = {
+                  value: output.states[state].value
+                };
+
+                console.log('PUT', addr, payload);
+
+                request.put({
+                  url: addr,
+                  body: payload,
+                  json: true,
+                  method: 'put'
+                }, function (error, response, body) {
+
+                  if (error && onError) {
+                    onError(error);
+                  }
+
+                  console.log(response.statusCode);
+
+                });
+
+
+              }
+            });
+
+          });
+        }
       }
     });
   };
