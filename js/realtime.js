9e961ee5c8d8732d1b49429f2e5909a0f44e1af1
jakebillings <jake@eakjb.com>
2016-10-21 16:16:12 -0600

    Listen on socket.io for notifications

--- js/realtime.js
+++ js/realtime.js
@@ -11,35 +11,50 @@ var transporter = nodemailer.createTransport({
   }
 });
 
+var sendNotification = function (notification,recipients) {
+  _io.emit('notification', notification);
+  recipients.forEach(function (recipient) {
+    console.log(recipient);
+
+    if (recipient.minPriority<=notification.priority) {
+      if (recipient.email) {
+        transporter.sendMail({
+
+          to: recipient.email,
+          from: "Eakjb Auto <eakjb.auto@gmail.com>",
+          subject: moment(notification.timestamp).format('MMM-DD-YYYY hh:mm A'),
+          text: notification.title + ': ' + (notification.body||'[No Content]'),
+          html: notification.title + ': ' + (notification.body||'[No Content]')
+
+        }, function (error, info) {
+          if (error) {
+            return console.log(error);
+          }
+          console.log('Message sent: ' + info.response);
+
+        });
+      }
+    }
+
+  });
+};
+
 module.exports = {
   init:function (io) {
     _io=io;
-  },
-  sendNotification: function (notification,recipients) {
-    _io.emit('notification', notification);
-    recipients.forEach(function (recipient) {
-      console.log(recipient);
 
-      if (recipient.minPriority<=notification.priority) {
-        if (recipient.email) {
-          transporter.sendMail({
-
-            to: recipient.email,
-            from: "Eakjb Auto <eakjb.auto@gmail.com>",
-            subject: moment(notification.timestamp).format('MMM-DD-YYYY hh:mm A'),
-            text: notification.title + ': ' + (notification.body||'[No Content]'),
-            html: notification.title + ': ' + (notification.body||'[No Content]')
-
-          }, function (error, info) {
-            if (error) {
-              return console.log(error);
-            }
-            console.log('Message sent: ' + info.response);
-
-          });
-        }
-      }
-
+    _io.on('connection', function (socket) {
+      socket.on('notification', function (notification) {
+        var models = require('./models');
+        models.Recipient.find({},function (error, result) {
+          if (error) {
+            console.log(error);
+            socket.emit('error', error);
+          }
+          sendNotification(notification,result);
+        });
+      });
     });
-  }
+  },
+  sendNotification: sendNotification
 };
